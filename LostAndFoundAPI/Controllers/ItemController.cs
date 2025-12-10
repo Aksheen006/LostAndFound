04dc3ded7a8e71bbbc9f3689ef3d2eef07b8ed67
gopiesa <gopiesa@sheridancollege.ca>
2025-12-09 22:49:40 -0500

    Removed Weather Controller and Class that were gnerated by default

---
+++ LostAndFoundAPI/Controllers/ItemController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LostAndFoundAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemController : ControllerBase
+    {
+    }
+}
